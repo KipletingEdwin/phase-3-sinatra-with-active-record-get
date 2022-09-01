064c3898be6ebbe5feca4c1866a0e7817f590d6c
Kipleting <kipletingedwin4@gmail.com>
2022-09-01 09:37:32 +0300

    commit tree 1ceabbf93138fb2a2bd0c05de23ecbc3fefe577f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  #Add thisline to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: {only: [:comment, :score], include:{
+        user: {only: [:name]}
+      } }
+    } )
+  
   end
 
 end
